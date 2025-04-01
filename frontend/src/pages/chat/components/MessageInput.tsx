e58d433187929c48b94551db6d35014d99b7be62
Amirakc2061@gmail.com <abhishekbhatta110@gmail.com>
2025-04-01 23:10:58 +0545

    File add

--- frontend/src/pages/chat/components/MessageInput.tsx
+++ frontend/src/pages/chat/components/MessageInput.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useChatStore } from "@/stores/useChatStore";
-import { useUser } from "@clerk/clerk-react";
 import { Send } from "lucide-react";
 import { useState } from "react";
 
 const MessageInput = () => {
 	const [newMessage, setNewMessage] = useState("");
-	const { user } = useUser();
 	const { selectedUser, sendMessage, currentUserId } = useChatStore();
 
 	const handleSend = () => {
